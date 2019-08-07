eca19995641e8d376e852d1aec79ef11179ebcf8
Frederic Collin <frederic.collin@gmail.com>
2019-08-07 20:56:43 +0200

    Initial commit

---
+++ src/main/java/io/ravioliinc/hsim/hsimapigateway/HSIMApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.ravioliinc.hsim.hsimapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSIMApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HSIMApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/ravioliinc/hsim/hsimapigateway/HsimApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package io.ravioliinc.hsim.hsimapigateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HsimApiGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
